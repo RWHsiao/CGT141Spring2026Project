4e21cf8f06e79f035e101d653aa35cb2072c21c4
RWHsiao <hsiao63@purdue.edu>
2026-04-03 11:09:44 -0400

    Updated href's for new domain

--- WWW/leaderboards.php
+++ WWW/leaderboards.php
@@ -7,7 +7,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <link href="/WWW/CSS/master.css" rel="stylesheet" type="text/css"/>
+        <link href="/CSS/master.css" rel="stylesheet" type="text/css"/>
     </head>
 
     <body>
